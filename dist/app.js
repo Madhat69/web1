f38f1cf524ccf19ac53592e77b24efcec6d694bf
Madhat69 <74453459+Madhat69@users.noreply.github.com>
2021-06-02 23:03:25 +0530

    Add files via upload

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+new fullpage("#fullpage", {
+  //options here
+  autoScrolling: true,
+  scrollHorizontally: true,
+  navigation: true,
+  navigationTooltips: ["Home", "About", "Work", "Contact"],
+  showActiveTooltip: false,
+  responsiveWidth: 767,
+});
